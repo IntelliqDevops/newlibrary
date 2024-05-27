e977564ffffefe7280104775fddbf246df8b6149
IntelliqDevops <155821476+IntelliqDevops@users.noreply.github.com>
2024-05-27 10:01:40 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -13,3 +13,9 @@ def buildArtifact()
 
 }
 
+def deployTomcat(jobname,ip,context)
+{
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
+}
+
+
